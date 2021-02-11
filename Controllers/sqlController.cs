cc57f02a6f459d0649c94794fb0b105d42ea25f5
Mike Synnott <synnott@gmail.com>
2021-02-11 12:17:24 +0000

    Tidied up code where DB types are cast to CLR types. Added code to handle TinyInt types on SQL Server for Linux

--- Controllers/sqlController.cs
+++ Controllers/sqlController.cs
@@ -1,15 +1,15 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Spiracle.NETCore.Models;
-using System.Text.Encodings.Web;
-using System.Data.SqlClient;
-using System.Data.Common;
-using System;
-using System.Collections.Generic;
-using Oracle.ManagedDataAccess.Client;
-
 namespace Spiracle.NETCore.Controllers
 {
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Spiracle.NETCore.Models;
+    using System.Data.SqlClient;
+    using System.Data.Common;
+    using System;
+    using System.Collections.Generic;
+    using Oracle.ManagedDataAccess.Client;
+
     public class sqlController : Controller
     {
         private readonly IConfiguration _configuration;
@@ -28,63 +28,63 @@ namespace Spiracle.NETCore.Controllers
         {
             var id = HttpContext.Request.Query["id"];
             string sqlQueryString = "SELECT * FROM users WHERE id = '" + id + "'";
-            return View("SpiracleUsersView", new SqlModel(executeMssqlQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteMsSqlQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult MsSql_Get_string()
         {
             var name = HttpContext.Request.Query["name"];
             string sqlQueryString = "SELECT * FROM users WHERE name = '" + name + "'";
-            return View("SpiracleUsersView", new SqlModel(executeMssqlQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteMsSqlQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult MsSql_Get_Implicit_Join()
         {
             var id = HttpContext.Request.Query["id"];
             string sqlQueryString = "SELECT * FROM users, address WHERE users.id = " + id + " AND users.id = address.id";
-            return View("SpiracleUsersView", new SqlModel(executeMssqlQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteMsSqlQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult MsSql_Get_Union()
         {
             var id = HttpContext.Request.Query["id"];
             string sqlQueryString = "SELECT name, surname, CONVERT(varchar(500),dob,3)  FROM users WHERE id = " + id + " UNION SELECT address_1, address_2, address_3 FROM address WHERE id = " + id;
-            return View("SpiracleUsersView", new SqlModel(executeMssqlQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteMsSqlQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult MsSql_Get_int_inline()
         {
             var id = HttpContext.Request.Query["id"];
             string sqlQueryString = id;
-            return View("SpiracleUsersView", new SqlModel(executeMssqlQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteMsSqlQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult MsSql_Get_int_no_quote()
         {
             var id = HttpContext.Request.Query["id"];
             string sqlQueryString = "select * from users where id=" + id;
-            return View("SpiracleUsersView", new SqlModel(executeMssqlQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteMsSqlQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult MsSql_Get_string_no_quote()
         {
             var name = HttpContext.Request.Query["name"];
             string sqlQueryString = "select * from users where name=" + name;
-            return View("SpiracleUsersView", new SqlModel(executeMssqlQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteMsSqlQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult MsSql_Get_string_param_question_mark()
         {
             var name = HttpContext.Request.Query["name"];
             string sqlQueryString = "SELECT top 5 id, name, surname FROM users where name <> '?' and name = '" + name + "'";
-            return View("SpiracleUsersView", new SqlModel(executeMssqlQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteMsSqlQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult MsSql_Implicit_Join_Namespace()
         {
             var id = HttpContext.Request.Query["id"];
             string sqlQueryString = "SELECT * FROM dbo.users, dbo.address WHERE dbo.users.id = " + id + " AND dbo.users.id = dbo.address.id";
-            return View("SpiracleUsersView", new SqlModel(executeMssqlQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteMsSqlQuery(sqlQueryString), sqlQueryString));
         }
 
 
@@ -92,77 +92,77 @@ namespace Spiracle.NETCore.Controllers
         {
             var id = HttpContext.Request.Query["id"];
             string sqlQueryString = "select * from users where id = '" + id + "'";
-            return View("SpiracleUsersView", new SqlModel(executeOracleQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteOracleQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult Oracle_Get_Full_Outer_Join()
         {
             var id = HttpContext.Request.Query["id"];
             string sqlQueryString = "SELECT * FROM users FULL OUTER JOIN address ON users.id = address.id AND users.id = " + id;
-            return View("SpiracleUsersView", new SqlModel(executeOracleQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteOracleQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult Oracle_Get_Implicit_Join()
         {
             var id = HttpContext.Request.Query["id"];
             string sqlQueryString = "SELECT * FROM users, address WHERE users.id = " + id + " AND users.id = address.id";
-            return View("SpiracleUsersView", new SqlModel(executeOracleQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteOracleQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult Oracle_Get_Union()
         {
             var id = HttpContext.Request.Query["id"];
             string sqlQueryString = "SELECT name, surname, TO_CHAR(dob) FROM users WHERE id = " + id + " UNION SELECT address_1, address_2, address_3 FROM address WHERE id = " + id;
-            return View("SpiracleUsersView", new SqlModel(executeOracleQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteOracleQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult Oracle_Get_int_groupby()
         {
             var id = HttpContext.Request.Query["id"];
             string sqlQueryString = "SELECT count(name), name FROM users GROUP BY " + id;
-            return View("SpiracleUsersView", new SqlModel(executeOracleQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteOracleQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult Oracle_Get_int_having()
         {
             var id = HttpContext.Request.Query["id"];
             string sqlQueryString = "SELECT MIN(name) from users GROUP BY id HAVING id = " + id;
-            return View("SpiracleUsersView", new SqlModel(executeOracleQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteOracleQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult Oracle_Get_int_inline()
         {
             var id = HttpContext.Request.Query["id"];
             string sqlQueryString = id;
-            return View("SpiracleUsersView", new SqlModel(executeOracleQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteOracleQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult Oracle_Get_int_no_quote()
         {
             var id = HttpContext.Request.Query["id"];
             string sqlQueryString = "select * from users where id = " + id;
-            return View("SpiracleUsersView", new SqlModel(executeOracleQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteOracleQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult Oracle_Get_int_orderby()
         {
             var id = HttpContext.Request.Query["id"];
             string sqlQueryString = "SELECT * FROM users ORDER BY " + id;
-            return View("SpiracleUsersView", new SqlModel(executeOracleQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteOracleQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult Oracle_Get_string()
         {
             var name = HttpContext.Request.Query["name"];
             string sqlQueryString = "select * from users where name = '" + name + "'";
-            return View("SpiracleUsersView", new SqlModel(executeOracleQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteOracleQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult Oracle_Get_string_no_quote()
         {
             var name = HttpContext.Request.Query["name"];
             string sqlQueryString = "select * from users where name = " + name;
-            return View("SpiracleUsersView", new SqlModel(executeOracleQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteOracleQuery(sqlQueryString), sqlQueryString));
         }
         
         public IActionResult Oracle_Get_string_no_quote_sanitised()
@@ -170,14 +170,14 @@ namespace Spiracle.NETCore.Controllers
             var name = HttpContext.Request.Query["name"];
             var newName = ((string)name).Replace("'", "''");
             string sqlQueryString = "select * from users where name = " + newName;
-            return View("SpiracleUsersView", new SqlModel(executeOracleQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteOracleQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult Oracle_Get_string_param_question_mark()
         {
             var name = HttpContext.Request.Query["name"];
             string sqlQueryString = "SELECT * FROM users where name <> '?' and name = '" + name + "'";
-            return View("SpiracleUsersView", new SqlModel(executeOracleQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteOracleQuery(sqlQueryString), sqlQueryString));
         }
 
         public IActionResult Oracle_Get_string_sanitised()
@@ -185,94 +185,70 @@ namespace Spiracle.NETCore.Controllers
             var name = HttpContext.Request.Query["name"];
             var newName = ((string)name).Replace("'", "''");
             string sqlQueryString = "select * from users where name = '" + newName + "'";
-            return View("SpiracleUsersView", new SqlModel(executeOracleQuery(sqlQueryString), sqlQueryString));
+            return View("SpiracleUsersView", new SqlModel(ExecuteOracleQuery(sqlQueryString), sqlQueryString));
         }
 
-        private List<SpiracleUser> executeMssqlQuery(string sqlQueryString)
+        private List<SpiracleUser> ExecuteMsSqlQuery(string sqlQueryString)
         {
-            String connectionString = _configuration.GetConnectionString("mssql");
-            SqlConnection conn = new SqlConnection(connectionString);
+            string connectionString = _configuration.GetConnectionString("mssql");
+            var conn = new SqlConnection(connectionString);
             conn.Open();
-            SqlCommand command = new SqlCommand(sqlQueryString, conn);
-            List<SpiracleUser> spiracleUsers = new List<SpiracleUser>();
+            var command = new SqlCommand(sqlQueryString, conn);
 
-            using (SqlDataReader reader = command.ExecuteReader())
-            {
-                spiracleUsers = createListOfSpiracleUsers(reader);
-            }
-
+            using SqlDataReader reader = command.ExecuteReader();
+            var spiracleUsers = CreateListOfSpiracleUsers(reader);
             conn.Close();
             return spiracleUsers;
         }
 
-        private List<SpiracleUser> executeOracleQuery(string sqlQueryString)
+        private List<SpiracleUser> ExecuteOracleQuery(string sqlQueryString)
         {
-            String connectionString = _configuration.GetConnectionString("oracle"); 
-            OracleConnection conn = new OracleConnection(connectionString);
+            var connectionString = _configuration.GetConnectionString("oracle"); 
+            var conn = new OracleConnection(connectionString);
             conn.Open();
-            OracleCommand command = new OracleCommand(sqlQueryString, conn);
-            List<SpiracleUser> spiracleUsers = new List<SpiracleUser>();
+            var command = new OracleCommand(sqlQueryString, conn);
 
-            using (OracleDataReader reader = command.ExecuteReader())
-            {
-                spiracleUsers = createListOfSpiracleUsers(reader);
-            }
-
+            using OracleDataReader reader = command.ExecuteReader();
+            var spiracleUsers = CreateListOfSpiracleUsers(reader);
             conn.Close();
             return spiracleUsers;
         }
 
-        private List<SpiracleUser> createListOfSpiracleUsers(DbDataReader reader)
+        private static List<SpiracleUser> CreateListOfSpiracleUsers(DbDataReader reader)
         {
-            List<SpiracleUser> spiracleUsers = new List<SpiracleUser>(); 
+            var spiracleUsers = new List<SpiracleUser>(); 
             
             while (reader.Read())
             {
-                SpiracleUser spiracleUser = new SpiracleUser();
+                var spiracleUser = new SpiracleUser();
 
-                for (int i = 0; i < reader.FieldCount; i++)
+                for (var i = 0; i < reader.FieldCount; i++)
                 {
-                    string colName = reader.GetName(i).ToLower();
+                    var colName = reader.GetName(i).ToLower();
                     switch (colName)
                     {
                         case "id":
-                            var idVal = reader.GetValue(i);
-                            // Id data type may be Decimal (Oracle XE) or int32 (ms sql server)
-                            if (idVal is Decimal)
-                            {
-                                spiracleUser.Id = (Decimal)reader.GetValue(i);
-                            }
-                            else if (idVal is int)
-                            {
-                                spiracleUser.Id = (int)reader.GetValue(i);
-                            }
+                            spiracleUser.Id = CastToInt(reader.GetValue(i));
                             break;
+
                         case "name":
-                            spiracleUser.Name = (reader.GetValue(i) is DBNull ? null : (string)reader.GetValue(i));
+                            spiracleUser.Name = CastToString(reader.GetValue(i));
                             break;
+
                         case "surname":
-                            spiracleUser.Surname = (reader.GetValue(i) is DBNull ? null : (string)reader.GetValue(i));
+                            spiracleUser.Surname = CastToString(reader.GetValue(i));
                             break;
+
                         case "dob":
-                            if (!reader.IsDBNull(i))
-                            {
-                                spiracleUser.Dob = (DateTime)reader.GetValue(i); ;
-                            }
+                            spiracleUser.Dob = CastToDateTime(reader, i);
                             break;
+
                         case "credit_card":
-                            spiracleUser.CreditCard = (reader.GetValue(i) is DBNull ? null : (string)reader.GetValue(i));
+                            spiracleUser.CreditCard = CastToString(reader.GetValue(i));
                             break;
+
                         case "cvv":
-                            var cvvVal = reader.GetValue(i);
-                            // Cvv data type may be Decimal (Oracle XE) or int32 (ms sql server)
-                            if (cvvVal is Decimal)
-                            {
-                                spiracleUser.Cvv = (Decimal)reader.GetValue(i);
-                            }
-                            else if (cvvVal is int)
-                            {
-                                spiracleUser.Cvv = (int)reader.GetValue(i);
-                            }
+                            spiracleUser.Cvv = CastToInt(reader.GetValue(i));
                             break;
                     }
                 }
@@ -281,6 +257,31 @@ namespace Spiracle.NETCore.Controllers
             }
             
             return spiracleUsers;
+        }
+
+        private static int CastToInt(object val)
+        {
+            return val switch
+            {
+                int intVal => intVal,
+                byte byteVal => byteVal,
+                decimal decimalVal => (int) decimalVal,
+                short shortVal => shortVal,
+                long longVal => (int) longVal,
+                _ => 0
+            };
+        }
+
+        private static string CastToString(object val)
+        {
+            if (val is DBNull) return null;
+            return (string) val;
+        }
+
+        private static DateTime CastToDateTime(DbDataReader reader, int fieldIndex)
+        {
+            if (reader.IsDBNull(fieldIndex)) return default;
+            return (DateTime)reader.GetValue(fieldIndex);
         }
     }
 }
--- Models/SpiracleUser.cs
+++ Models/SpiracleUser.cs
@@ -5,21 +5,21 @@ namespace Spiracle.NETCore.Models
 {
     public class SpiracleUser
     {
-        public Decimal Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public DateTime Dob { get; set; }
         public string CreditCard { get; set; }
-        public Decimal Cvv { get; set; }
+        public int Cvv { get; set; }
 
-        public SpiracleUser(Object[] userData)
+        public SpiracleUser(object[] userData)
         {
-            Id = (Decimal) userData[0];
+            Id = (int) userData[0];
             Name = (string) userData[1];
             Surname = (string) userData[2];
             Dob = (DateTime) userData[3];
             CreditCard = (string) userData[4];
-            Cvv = (Decimal) userData[5];
+            Cvv = (int) userData[5];
         }
 
         public SpiracleUser(){}
