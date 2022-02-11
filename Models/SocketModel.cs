6e7c97fcfdfc2dad689da819640d1cf59f9d9b03
mkennedy <mkennedy@waratek.com>
2022-02-11 16:13:02 +0000

    QA-1406. Add input field for server socket IP to Spiracle.NetCore Socket page

--- Models/SocketModel.cs
+++ Models/SocketModel.cs
@@ -6,6 +6,7 @@ namespace Spiracle.NETCore.Models
     public class SocketModel
     {
         public string ServerIP { get; set; }
+        public string LocalServerListeningIP { get; set; }
         public string LocalServerListeningPort { get; set; }
         public string ServerPort { get; set; }
         public string TextData { get; set; }
